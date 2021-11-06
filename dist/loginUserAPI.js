13ceb11278abf7b8545f34fcd78b7854058eb3ec
GuilleFerru <guillelf@gmail.com>
2021-11-06 16:35:34 -0300

    desafio 28

--- dist/loginUserAPI.js
+++ dist/loginUserAPI.js
@@ -46,8 +46,8 @@ var connect_mongo_1 = __importDefault(require("connect-mongo"));
 var cookie_parser_1 = __importDefault(require("cookie-parser"));
 var passport_1 = __importDefault(require("passport"));
 var passport_facebook_1 = require("passport-facebook");
-var FACEBOOK_CLIENT_ID = process.argv[3] || '1280074459156595';
-var FACEBOOK_CLIENT_SECRET = process.argv[4] || '27d2001ec573f933251c8d2d61b61434';
+var FACEBOOK_CLIENT_ID = process.argv[3] || '423519862624165';
+var FACEBOOK_CLIENT_SECRET = process.argv[4] || 'de42abdb2f8e3917e10682d189668d1f';
 passport_1.default.use(new passport_facebook_1.Strategy({
     clientID: FACEBOOK_CLIENT_ID,
     clientSecret: FACEBOOK_CLIENT_SECRET,
--- ts/loginUserAPI.ts
+++ ts/loginUserAPI.ts
@@ -11,8 +11,8 @@ declare module 'express-session' {
     }
 }
 
-const FACEBOOK_CLIENT_ID = process.argv[3] || '1280074459156595';
-const FACEBOOK_CLIENT_SECRET = process.argv[4] || '27d2001ec573f933251c8d2d61b61434';
+const FACEBOOK_CLIENT_ID = process.argv[3] || '423519862624165';
+const FACEBOOK_CLIENT_SECRET = process.argv[4] || 'de42abdb2f8e3917e10682d189668d1f';
 
 passport.use(new FacebookStrategy({
     clientID: FACEBOOK_CLIENT_ID,

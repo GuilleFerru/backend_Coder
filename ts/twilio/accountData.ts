2fdab783bd35b5598a06cc7c9adb300542182665
Guille <guillelf@gmail.com>
2021-12-04 13:06:43 -0300

    Update accountData.ts

--- ts/twilio/accountData.ts
+++ ts/twilio/accountData.ts
@@ -1,3 +1,3 @@
-// Wilson%123456789
-export const accountSid = 'AC1471a99c86fe7bd44086726a5ae428eb';
-export const authToken = '3516780320daeeb542db92f03e884079';
+
+export const accountSid = '';
+export const authToken = '';
